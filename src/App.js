bee05cc2d4ef9cdf85f72ef400d743c9041e95b1
Jakana Kiwanuka <jakanakiwanuka@Jakanas-MacBook-Pro.local>
2018-03-12 18:37:54 +0300

    Created Login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import ParamsExample from './components/params';
 import Login from './components/login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Login />
+
       </div>
     );
   }
--- src/components/login.js
+++ src/components/login.js
@@ -1,23 +1,52 @@
-import React from "react";
-import {Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-//css goes here
+import React, {Component} from "react";
+import {Button, FormGroup, FormControl, ControlLabel,Label} from "react-bootstrap";
+import "./login/login.css";
 
-const Login = () => {
-    return (
-      <div classname="Login">
-        {/* <form>
-          <FormGroup controlId="email" bsSize
-        </form> */}
-        <Button
-            block
-            bsSize="large"
-            type="submit">
-            Login
-          </Button>
+class Login extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+      username:"",
+      password:""
+    };
+  }
+
+  render() {
+    return(
+      <div className="wrapper">
+        <form className="form-signin">
+          <h2 className="form-signin-heading">Please login</h2>
+          <FormGroup
+            controlID="formBasicText"
+          >
+            <FormControl className="nameField"
+              autoFocus
+              type="username"
+              value={this.state.username}
+              // onChange
+              placeholder="Enter Username"
+            />
+            <FormControl className="passwordField"
+              autofocus
+              type="password"
+              value={this.state.password}
+              // onchange
+              placeholder="Enter Username"
+            />
+            <Button bsStyle="primary" bsSize="large" block>
+              Login
+            </Button>
+
+            <FormControl.Feedback />
+          </FormGroup>
+        </form>
+
+
+
       </div>
-    )
+    );
+  }
 }
-
-// export function validateform()
 
 export default Login;
