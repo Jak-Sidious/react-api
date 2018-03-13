84f672bc223400cc0f0d1be11b60ffc17341952b
Jakana Kiwanuka <jakanakiwanuka@Jakanas-MacBook-Pro.local>
2018-03-14 01:09:52 +0300

    Implemented first steps of simple transitions before actually functionality

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,19 +1,33 @@
 import React from "react";
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from "react-bootstrap";
+import FlipPage from "react-flip-page";
+import PropTypes from "prop-types";
 
 class Landing extends React.Component{
   constructor(props){
     super(props);
 
     this.state={
-
+      
     };
   }
 
   render() {
     return(
-      <div className="landing">
-        
+      <div>
+        <FlipPage >
+          <article>
+            <h1>My awesome first article</h1>
+            <p>My awesome first content</p>
+          </article>
+          <article>
+            <h1>My wonderful second article</h1>
+            <p>My wonderful second content</p>
+          </article>
+          <article>
+            <h1>My excellent third article</h1>
+            <p>My excellent third content</p>
+          </article>
+        </FlipPage>
       </div>
     );
   }
