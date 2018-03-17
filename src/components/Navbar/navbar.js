fc7f52990f1c2b62cd76954988de5435b171a488
Jakana Kiwanuka <jakanakiwanuka@Jakanas-MacBook-Pro.local>
2018-03-17 20:13:00 +0300

    Create navbar stub

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import { Input, Menu } from 'semantic-ui-react'
 
 
-const Navigation = () => {
-  return(
-    <div>
-      
-    </div>
-  )
-};
+class Navigation extends Component {
+  state = {
+    activeItem: 'home'
+  }
 
-export default Navigation;
+  handleItemClick = (event, { name }) => {
+    this.setState({ activeItem: name })
+  }
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div>
+        <Menu inverted>
+        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+        <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search...' />
+          </Menu.Item>
+          <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
+        </Menu.Menu>
+      </Menu>
+      </div>
+    )
+  }
+}
+
+export default Navigation
--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Form, Button } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Form, Button } from 'semantic-ui-react';
 import axiosInstance from '../commonComponents/AxiosInstance';
 
 
