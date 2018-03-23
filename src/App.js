d96c452ab6aaa25fdd3696cf3cda52a76abe519d
Jakana Kiwanuka <jakanakiwanuka@Jakanas-MacBook-Pro.local>
2018-03-23 10:59:12 +0300

    Implement route protection

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 // components
+import { Protected } from './components/commonComponents/Protected';
 import Login from './components/Login/login';
 import Register from './components/Register/register';
 import Landing from './components/Landing/landing';
@@ -16,14 +17,14 @@ class App extends Component {
       <div className="App">
         <BrowserRouter>
           <div>
-            <Route
+            <Protected
               exact
               path="/category/:category_id/recipes/list"
               component={viewRecipies}
             />
-            <Route exact path="/viewCat" component={viewCategories} />
-            <Route exact path="/catCreate" component={createCategory} />
-            <Route exact path="/landing" component={Landing} />
+            <Protected exact path="/viewCat" component={viewCategories} />
+            <Protected exact path="/catCreate" component={createCategory} />
+            <Protected exact path="/landing" component={Landing} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/" component={Register} />
           </div>
